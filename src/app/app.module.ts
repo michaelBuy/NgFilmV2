06d01adda2e0f645b257daf540855a2af55e3cb1
Michael Buyens <michaelbuyens@live.com>
2022-12-08 14:10:53 +0100

    en cours

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FilmListComponent } from './film-list/film-list.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilmListComponent,
+    NavbarComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/film-list/film-list.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FilmListRoutingModule } from './film-list-routing.module';
+import { FilmListComponent } from './film-list.component';
+
+
+@NgModule({
+  declarations: [
+    
+  ],
+  imports: [
+    CommonModule,
+    FilmListRoutingModule
+  ]
+})
+export class FilmListModule { }
